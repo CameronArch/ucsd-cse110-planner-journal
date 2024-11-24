6eb24e851dda4691325f5cd7710dbe365b46a6d8
CameronArch <cameronarch598@gmail.com>
2024-11-23 20:20:43 -0800

    Added Test case for return to planner button

--- src/Components/DisplayUI/DisplayCalendarJournal.tsx
+++ src/Components/DisplayUI/DisplayCalendarJournal.tsx
@@ -2,8 +2,6 @@ import { useContext } from "react";
 import { JournalPageContext } from "../JournalPage/JournalPageContext";
 import CalendarPage from "../CalendarPage/Calendar-Page";
 import JournalPage from "../JournalPage/Journal-Page";
-import { TaskMenuContextProvider } from "../TaskMenu/TaskMenuContext";
-import { MonthChangeContextProvider } from "../MonthChangeButton/MonthChangeContext";
 import RemindersManager from "../ReminderPopup/RemindersManager"
 
 const DisplayCalendarJournal = () => {
