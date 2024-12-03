5cbe993a41578db728f29579e406fab8c25b69d2
CameronArch <cameronarch598@gmail.com>
2024-12-02 16:13:09 -0800

    Fixed wront file path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // src/App.tsx
 import { AccountContextProvider } from "./Components/LoginSignUpPage/AccountContext";
 import { Amplify } from 'aws-amplify';
-import outputs from '../amplify_outputs.json';
+import outputs from './amplify_outputs.json';
 import Login from "./Components/LoginSignUpPage/Login";
 
 Amplify.configure(outputs);
